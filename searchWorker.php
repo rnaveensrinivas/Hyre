4e882d9074d4a32ecb7fc73752a9f346ac67a686
Your Name <you@example.com>
2022-05-28 17:48:11 +0530

    back end 50%

--- searchWorker.php
+++ searchWorker.php
@@ -1,5 +1,143 @@
 <?php
+session_start() ; 
+include 'config.php' ; 
 
-    //only for client side.
+if( $_SESSION['userType'] == "C" ){
+    if(isset($_POST['submit'])){ 
 
+
+
+        $pincode = $_POST['pincode']; 
+        $gender = $_POST['gender'];
+        $jobType = $_POST['jobType']; 
+        echo"$gender" ;
+
+        if( $gender == "NULL"){
+            if( $jobType == "NULL"){
+                //should be changed to view.
+                $selectWorkers = "SELECT * FROM worker,account where worker.workerID=account.ID and pincode = '$pincode' ";
+            }else{
+                $selectWorkers = "SELECT * FROM worker,account where worker.workerID=account.ID and pincode = '$pincode' and jobType = '$jobType' ";
+            }
+        }else{
+            if( $jobType == "NULL"){
+                //should be changed to view.
+                $selectWorkers = "SELECT * FROM worker,account where worker.workerID=account.ID and pincode = '$pincode' and gender = '$gender' " ;
+            }else{
+                $selectWorkers = "SELECT * FROM worker,account where worker.workerID=account.ID and pincode = '$pincode' and gender = '$gender' and jobType = '$jobType' ";
+            }
+        }
+
+        if ( $result = mysqli_query( $conn, $selectWorkers ) ) { 
+            while ( $row = mysqli_fetch_assoc($result) ) { 
+                $printName = $row['name'] ;
+                $printAverageRating = $row['averageRating'] ;
+                $printExperience = $row['experience'] ;
+                $workerID = $row['workerID'] ; 
+                echo "<h3>Name : $printName "; 
+                echo "<p>Average Rating : $printAverageRating<br>Experience : $printExperience</p>" ;
+        
+                echo "<a href='workerProfile.php?=$workerID' id='submit-button'><button>Worker Profile</button></a></h3>" ;
+                //Joining a specific team page. And we are passing the team name using GET to that teams page.
+            }
+        }
+        else{ 
+            //echo "<script>alert('You have to join a new team.')</script>" ; 
+        }
+
+
+
+
+
+
+        /*
+        $TeacherID = $_SESSION['CollegeID'] ; 
+        $TeamName = $_POST['TeamName'] ;
+        $TeamName .= "_" ; 
+        $TeamName .= $TeacherID ; //So that the team names for a teacher remains unique.
+
+        //Checking if the class exists or not. 
+        $query = "SELECT * FROM teams WHERE TeamName = '$TeamName' " ; 
+        $result = $conn->query($query) ; 
+
+        if( $result->num_rows ){
+            //Team already exists.
+            $error .= "This team Name already exists.Try again." ; 
+        }
+        else{
+            $TeacherName = $_SESSION['FullName']  ;
+
+            $Keycode = md5(time().$TeamName) ; //Creating an encryption Keycode.
+            $Keycode = substr($Keycode,0,10) ; //Taking only the first 10 char of encryption created. 
+
+            $query = "INSERT INTO teams(TeamName, TeacherName , TeacherID , Keycode ) VALUES('$TeamName', '$TeacherName' , '$TeacherID' , '$Keycode')" ; 
+            $result = $conn->query($query) ; 
+
+            if( $result ){
+                echo"<script>alert('Team channel created succesfully.Redirecting to main lobby.') </script>" ; 
+                echo"<script>document.location='mainlobby.php'</script>" ;
+            }
+            else{ 
+                //echo mysqli_error($conn);
+                echo"<script>alert('Unable to create team channel.Redirecting to main lobby.') </script>" ; 
+                echo"<script>document.location='mainlobby.php'</script>" ; 
+            }
+        }
+        */
+    }
+    $conn->close();
+}
+else{ 
+    header("location:index.html") ; 
+}
 ?>
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Search Workers</title>
+        <link rel="stylesheet" type="text/css" href="1Level/style2.css">
+    </head>
+
+    <body>
+        
+        <div class="logout">
+            <button type="button" onclick="location.href='logout.php'" name="Logout" id="submit-button" style="background-color: white; color:rgb(95, 108, 255);">Sign Out</button>
+        </div>
+        <form method="POST" action="" autocomplete="off">
+            <div class="form">
+                <h2>(to be filled)</h2>
+                <p style="color:red; line-height: 120%;"> <?php echo $error ; ?></p>
+
+                <div class="email">
+                <label for="pincode">Pincode</label><br>
+                <input type="number" id="pincode" name="pincode" min="100000" max="999999" placeholder="Eg: 600025" required><br>
+                </div>
+
+                <label for="jobType">Type of job:</label>
+                <select name="jobType" id="jobType" >
+                <option value="NULL" selected disabled hidden>Select an Option</option>
+                <option value="Carpenter">Carpenter</option>
+                <option value="Cook">Cook</option>
+                <option value="Maid">Maid</option>
+                <option value="Painter">Painter</option>
+                </select><br>
+
+                <label for="gender">Gender:</label>
+                <select name="gender" id="gender" >
+                <option value="NULL" selected disabled hidden>Select an Option</option>
+                <option value="M">Male</option>
+                <option value="F">Female</option>
+                <option value="O">Other</option>
+                </select>
+
+
+            
+
+
+                <button type="submit" name="submit" id="submit-button">Search</button>
+            </div> 
+        </form>
+    </body>
+ </html> 
---
+++ workerProfile.php
@@ -0,0 +1,96 @@
+<?php
+session_start() ; 
+include 'config.php' ; 
+
+if( $_SESSION['userType'] == "C" ){
+    $workerID = "" ;
+    if( isset($_GET['ID']) ){
+        $workerID = $_GET['id'] ; 
+    }
+    else{
+        //bring in java script here with alernt and then redirect.
+        header("location:searchWorker.php") ;
+    }
+
+    
+    $selectWorkers = "SELECT * FROM worker,account where worker.workerID=account.ID and workerID = '$workerID' ";
+    $result = mysqli_query( $conn, $selectWorkers ) ;
+    $row = mysqli_fetch_assoc($result) ;
+
+    //for now displaying just the name and some necessary details. 
+
+    $printName = $row['name'] ;
+    $printAverageRating = $row['averageRating'] ;
+    $printExperience = $row['experience'] ;
+    $workerID = $row['workerID'] ; 
+    echo "<h3>Name : $printName "; 
+    echo "<p>Average Rating : $printAverageRating<br>Experience : $printExperience</p>" ;
+?>
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Search Workers</title>
+        <link rel="stylesheet" type="text/css" href="1Level/style2.css">
+    </head>
+
+    <body>
+        
+        <div class="logout">
+            <button type="button" onclick="location.href='logout.php'" name="Logout" id="submit-button" style="background-color: white; color:rgb(95, 108, 255);">Sign Out</button>
+        </div>
+            <div class="form">
+                <h2>(to be filled)</h2>
+                <h3>Worker Profile below</h3>
+                
+            </div> 
+        
+    </body>
+ </html> 
+
+ <?php
+
+        
+
+        /*
+        $TeacherID = $_SESSION['CollegeID'] ; 
+        $TeamName = $_POST['TeamName'] ;
+        $TeamName .= "_" ; 
+        $TeamName .= $TeacherID ; //So that the team names for a teacher remains unique.
+
+        //Checking if the class exists or not. 
+        $query = "SELECT * FROM teams WHERE TeamName = '$TeamName' " ; 
+        $result = $conn->query($query) ; 
+
+        if( $result->num_rows ){
+            //Team already exists.
+            $error .= "This team Name already exists.Try again." ; 
+        }
+        else{
+            $TeacherName = $_SESSION['FullName']  ;
+
+            $Keycode = md5(time().$TeamName) ; //Creating an encryption Keycode.
+            $Keycode = substr($Keycode,0,10) ; //Taking only the first 10 char of encryption created. 
+
+            $query = "INSERT INTO teams(TeamName, TeacherName , TeacherID , Keycode ) VALUES('$TeamName', '$TeacherName' , '$TeacherID' , '$Keycode')" ; 
+            $result = $conn->query($query) ; 
+
+            if( $result ){
+                echo"<script>alert('Team channel created succesfully.Redirecting to main lobby.') </script>" ; 
+                echo"<script>document.location='mainlobby.php'</script>" ;
+            }
+            else{ 
+                //echo mysqli_error($conn);
+                echo"<script>alert('Unable to create team channel.Redirecting to main lobby.') </script>" ; 
+                echo"<script>document.location='mainlobby.php'</script>" ; 
+            }
+        }
+        */
+    
+        $conn->close();
+    }
+    else{ 
+        header("location:index.html") ; 
+    }
+    ?>
