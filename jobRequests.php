d88f9612ee26cfc7c965f9cbed50fc1feb943a17
Your Name <you@example.com>
2022-05-30 14:06:57 +0530

    Job Requests and View Requests done

--- jobRequests.php
+++ jobRequests.php
@@ -18,97 +18,42 @@
 session_start() ; 
 include 'config.php' ; 
 
-// Displaying student main lobby
 if( $_SESSION['userType'] == "W"){ 
 
-    //To get the student table name. 
     $workerID = $_SESSION['ID'] ; 
 
-    //For displaying all the teams they have enrolled in. 
     $selectAllRequests = "SELECT * FROM job where workerID='$workerID' and bookingStatus = 0" ; 
     if ( $result = mysqli_query( $conn, $selectAllRequests ) ) { 
         while ( $row = mysqli_fetch_assoc($result) ) { 
 
-            $printClientID = $row['clientID'] ;
-            $printDescription = $row['description'] ;  
+            $clientID = $row['clientID'] ;
+            $description = $row['description'] ;  
             $jobID = $row['jobID'] ; 
 
-            echo "<h3>Client ID : $printClientID<br>Description : $printDescription<br>"; 
-            echo "<form action='' method='POST'>";
-            echo "<input type='submit' value='Accept' name='accept' id='submit-button'>" ; 
-            echo "<input type='submit' value='Reject' name='reject' id='submit-button'>" ;
-            echo "</form>" ;
-            if( isset($_POST['accept'])){
-                $_POST = array() ;
-                $acceptRequestQuery = "update job set bookingStatus = 1, jobStatus = 1 where jobID ='$jobID'" ;
-                if( $result = mysqli_query( $conn, $acceptRequestQuery ) ){
-                    echo "<script>alert('Request for $printClientID accepted.')</script>" ;
-                    echo "<script>location.reload()</script>" ;
-                }
-            }
-            else if( isset($_POST['reject'])){
-                $_POST = array() ;
-                $rejectRequestQuery = "update job set bookingStatus = 2, jobStatus = 2 where jobID ='$jobID'" ;
-                if( $result = mysqli_query( $conn, $rejectRequestQuery ) ){
-                    echo "<script>alert('Request for $printClientID canceled.')</script>" ;
-                    echo "<script>location.reload()</script>" ;
-                }
-            }
-            //echo "<a href='teams.php?TeamName=$teams' id='submit-button'><button> Join </button></a></h3>" ;
-            //Joining a specific team page. And we are passing the team name using GET to that teams page.
+            
+            echo "<h3>Client ID : $ClientID<br>Description : $Description<br>"; 
+            echo "<a href='viewRequest.php?jobID=$jobID&workerID=$workerID&clientID=$clientID' id='submit-button'><button>View Request</button></a></h3>" ;
         }
     }
-    else{ 
-        //echo "<script>alert('You have to join a new team.')</script>" ; 
-    }
+    
     $conn->close();
-    //Joining team below. 
-?>
-
-    <!--<button onclick="location.href='jointeam.php'" id="submit-button">Join Team</button> -->
-
-<?php
-
-}   // Displaying teacher main lobby
+}   
 else if( $_SESSION['userType'] == "C"){ 
 
-    //To get the student table name. 
     $clientID = $_SESSION['ID'] ; 
 
-    //For displaying all the teams they have enrolled in. 
     $selectAllRequests = "SELECT * FROM job where clientID='$clientID' and bookingStatus = 0" ; 
     if ( $result = mysqli_query( $conn, $selectAllRequests ) ) { 
         while ( $row = mysqli_fetch_assoc($result) ) { 
 
-            $printWorkerID = $row['workerID'] ;
-            $printDescription = $row['description'] ;  
+            $workerID = $row['workerID'] ;
+            $description = $row['description'] ;  
             $jobID = $row['jobID'] ; 
 
-            echo "<h3>Worker ID : $printWorkerID<br>Description : $printDescription<br>"; 
-            echo "<form action='' method='POST'>";
-            echo "<input type='submit' value='Cancel Request' name='cancelRequest' id='submit-button'>" ; 
-            //echo "<input type='submit' value='Reject' name='reject' id='submit-button'>" ;
-            echo "</form>" ;
-            if( isset($_POST['cancelRequest'])){
-                unset($_POST['cancelRequest']) ;
-                $withdrawRequestQuery = "update job set bookingStatus = 3, jobStatus = 2 where jobID ='$jobID'" ;
-                if( $result = mysqli_query( $conn, $withdrawRequestQuery ) ){
-                    echo "<script>alert('Request for $printWorkerID has been withdrawn.')</script>" ;
-                    echo "<script>location.reload()</script>" ;
-                }
-            }
-            //echo "<a href='teams.php?TeamName=$teams' id='submit-button'><button> Join </button></a></h3>" ;
-            //Joining a specific team page. And we are passing the team name using GET to that teams page.
+            echo "<h3>Worker ID : $workerID<br>Description : $description<br>"; 
+            echo "<a href='viewRequest.php?jobID=$jobID&workerID=$workerID&clientID=$clientID' id='submit-button'><button>View Request</button></a></h3>" ;
         }
     }
-    else{ 
-        //echo "<script>alert('You have to join a new team.')</script>" ; 
-    }
-?>
-
-    <!--<button onclick="location.href='createteam.php'" id='submit-button'>Create Team</a></button>-->
-
-<?php
 $conn->close();
 }else{ 
 //Invalid access detected.
---
+++ viewRequest.php
@@ -0,0 +1,165 @@
+<?php
+
+session_start() ; 
+include 'config.php' ; 
+
+$workerID = "" ;
+$clientID = "" ;
+$jobID = "" ; 
+
+if( isset($_GET['workerID'] ) ){ 
+    $workerID = $_GET['workerID'] ; 
+}else{
+    header("location:jobRequests.php") ; 
+}
+
+if( isset($_GET['clientID'] ) ){ 
+    $clientID = $_GET['clientID'] ; 
+}else{
+    header("location:jobRequests.php") ; 
+}
+
+if( isset($_GET['jobID'] ) ){ 
+    $jobID = $_GET['jobID'] ; 
+}else{
+    header("location:jobRequests.php") ; 
+}
+
+$selectAllRequests = "SELECT * FROM job where jobID = '$jobID'" ; 
+$result = mysqli_query( $conn, $selectAllRequests ) ;
+$row = mysqli_fetch_assoc($result) ;
+
+$landmark = $row['landmark'] ; 
+$pincode = $row['pincode'] ; 
+$time = $row['time'] ; 
+$date = $row['date'] ;
+$workType = $row['workType'] ;
+$description =  $row['description'] ; 
+
+if( $_SESSION['userType'] == "C" ){
+
+    if(isset($_POST['withdrawRequest'])){ 
+    
+        $withdrawRequestQuery = "update job set bookingStatus = 3, jobStatus = 2 where jobID ='$jobID'" ;
+        if( $result = mysqli_query( $conn, $withdrawRequestQuery ) ){
+            echo "<script>alert('Request for $workerID has been withdrawn. Redirecting you to job requests page.')</script>" ;
+            header("location:jobRequests.php") ; 
+        }else{
+            echo "<script>alert('Unable to withdraw request for worker $workerID.')</script>" ;
+        }
+        $conn->close();
+
+    }   
+}
+else if( $_SESSION['userType'] == "W" ){
+
+    if( isset($_POST['acceptRequest'])){
+        $acceptRequestQuery = "update job set bookingStatus = 1, jobStatus = 1 where jobID ='$jobID'" ;
+        if( $result = mysqli_query( $conn, $acceptRequestQuery ) ){
+            echo "<script>alert('Request for $clientID accepted.')</script>" ;
+            header("location:jobRequests.php") ; 
+        }
+    }
+    else if( isset($_POST['rejectRequest'])){
+        $rejectRequestQuery = "update job set bookingStatus = 2, jobStatus = 2 where jobID ='$jobID'" ;
+        if( $result = mysqli_query( $conn, $rejectRequestQuery ) ){
+            echo "<script>alert('Request for $clientID canceled.')</script>" ;
+            header("location:jobRequests.php") ; 
+        }
+    }
+
+    $conn->close();
+
+}
+    
+
+else{ 
+    header("location:index.html") ; 
+}
+
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Request</title>
+    <link rel="stylesheet" type="text/css" href="1Level/darkTheme.css">
+    <script src="1Level/validation.js"></script>
+  </head>
+
+
+  <body onload="newCaptcha()">
+    <form action="" method="POST" autocomplete="off" >
+      <div class="form">
+
+        <h2>Job Request</h2>
+
+        <?php 
+        if( $_SESSION['userType'] == "C" ){
+        ?>
+        <div class="fname">
+          <label for="workerID">Worker ID</label><br>
+          <input type = "text" id="workerID" name="workerID" value='<?php echo $workerID ?>'readonly> <br>
+        </div>
+        <?php 
+        }
+        ?>
+
+        <?php 
+        if( $_SESSION['userType'] == "W" ){
+        ?>
+        <div class="fname">
+          <label for="clientID">Client ID</label><br>
+          <input type = "text" id="clientID" name="clientID" value='<?php echo $clientID ?>'readonly> <br>
+        </div>
+        <?php 
+        }
+        ?>
+
+        <div class="fname">
+          <label for="landmark">Landmark</label><br>
+          <input type = "text" id="landmark" name="landmark" value='<?php echo $landmark ?>'readonly> <br>
+        </div>
+
+        <div class="fname">
+          <label for="pincode">Pincode</label><br>
+          <input type = "text" id="pincode" name="pincode" value='<?php echo $pincode ?>'readonly> <br>
+        </div>
+
+        <div class="fname">
+          <label for="time">Time</label><br>
+          <input type = "text" id="time" name="time" value='<?php echo $time ?>'readonly> <br>
+        </div>
+
+        <div class="fname">
+          <label for="date">Date</label><br>
+          <input type = "text" id="date" name="date" value='<?php echo $date ?>'readonly> <br>
+        </div>
+
+
+        <label for="description">Work description</label><br>
+        <textarea id="description" name="description" required rows="10" cols="40" readonly><?php echo $description ?></textarea><br>
+
+        <?php 
+
+
+
+        if( $_SESSION['userType'] == "C" ){
+        ?>
+        <button type="submit" name="withdrawRequest" id="submit-button">Withdraw Request</button>
+        <?php 
+        }
+        else
+        if( $_SESSION['userType'] == "W" ){
+        ?>
+        <button type="submit" name="acceptRequest" id="submit-button">Accept</button>
+        <button type="submit" name="rejectRequest" id="submit-button">Reject</button>
+        <?php 
+        }
+        ?>
+      </div> 
+    </form>
+  </body>
+</html>
