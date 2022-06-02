a885deaabedbcb0b9e368fb038d846349bece368
Your Name <you@example.com>
2022-06-02 18:36:44 +0530

    serach worker

--- searchWorker.php
+++ searchWorker.php
@@ -97,6 +97,12 @@ if( $_SESSION['userType'] == "C" ){
             }
             if ( $result = mysqli_query( $conn, $selectWorkers ) ) { 
                 echo "<div class='search-worker'>" ; 
+
+                //center align this please, give a good English sentence for this.
+                echo "<p>Workers available in $pincode<p>"; 
+
+
+
                 while ( $row = mysqli_fetch_assoc($result) ) { 
                     $isAvailableWorker = 1 ;
                     $printName = $row['name'] ;
@@ -104,10 +110,11 @@ if( $_SESSION['userType'] == "C" ){
                     $printExperience = $row['experience'] ;
                     $jobType = $row['jobType'] ;
                     $workerID = $row['workerID'] ; 
+                    $gender = $row['gender'] ;
                     echo "<div class='card' style='width:300px; margin: 20px; float:left'>" ;
                     echo "<div class='card-body' style='text-align:center; '>";
                     echo "<h5 class='card-title' >$printName</h5>"; 
-                    echo "<p class='card-text' style='font-weight:300; font-size:1.25rem'>Job Type: $jobType<br>Average Rating: $printAverageRating<br>Experience: $printExperience<br></p>" ;
+                    echo "<p class='card-text' style='font-weight:300; font-size:1.25rem'>Gender: $gender<br>Job Type: $jobType<br>Average Rating: $printAverageRating<br>Experience: $printExperience<br></p>" ;
                     echo "<a href='workerProfile.php?workerID=$workerID' id='submit-button' style='text-decoration:none;'><button style='width:50%; border-radius:7px; margin:auto; display:block;'>View Worker</button></a></h3>" ;
                     echo "</div>" ;
                     echo "</div>" ;
